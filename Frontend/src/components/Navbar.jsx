f029b6ef1824dec04acb763150c564e795f553e8
sxhilx <jadasahil786@gmail.com>
2025-02-28 14:06:58 +0200

    Logo added

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Logo } from '../assets'
 
 const Navbar = () => {
   return (
     <div className='w-full bg-transparent border-b border-gray-300 font-primary'>
         <div className='max-w-7xl px-2 mx-auto flex justify-between py-3'>
             <div className='flex justify-center items-center'>
+                <img src={Logo} alt="Logo" className='w-9' />
                 <h1 className='text-xl font-semibold'>JobsVault</h1>
             </div>
             <div className='flex justify-center items-center gap-6'>
